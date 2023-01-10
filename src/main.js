cede2ea85d2faf31895a5abed12d941e88ceef57
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-10 10:32:23 +0000

    Update 5 files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'normalize.css/normalize.css'
-import 'prismjs/themes/prism.css'
-import 'prismjs/themes/prism-okaidia.css'
 import './assets/tables.scss'
 
 import Vue from 'vue'
