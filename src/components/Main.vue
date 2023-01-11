824f4308e5b41a3a5ee10647abd21ddff81896c4
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-11 06:08:37 +0000

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,225 +1,300 @@
 <template>
-  <div class="main-section">
-    <h3 class="section-title teko white">
-      How to use
-    </h3>
-    <pre class="npm-code white">
-              <code>
-npm install vue-particles --save-dev
-              </code>
-     </pre>
+  <div
+  class="w-full">
+    <!-- Hero section -->
+    <section id="hero" class="md:justify-end flex flex-col justify-center pt-20 pb-8 lg:overflow-hidden relative mb-20">
+      <BaseSection>
+        <div class="col-span-12 lg:col-span-8 mt-10 xl:mt-10 space-y-4 sm:space-y-4 px-1 text-center sm:text-left">
+          <span class="text-base text-gradient font-semibold uppercase"
+            >Join us Today</span
+          >
+          <h1
+            class="text-[2.5rem] sm:text-5xl xl:text-6xl font-bold leading-tight capitalize sm:pr-8 xl:pr-10"
+          >
+            The World's <span class="text-header-gradient">First AI supported</span>  Telegram Bot
+          </h1>
+          <p  class="paragraph hidden sm:block">
+            <b>J</b>ust <b>A</b> <b>R</b>ather <b>V</b>ery <b>I</b>ntelligent <b>S</b>ystem
+          </p>
+          <div 
+            class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mt-2"
+          >
+          <div class="flex justify-start space-x-6 socials mt-0 pb-0 text-dark">
+            <a href="https://t.me/beeperc" target="_blank" rel="noopener noreferrer">
+              <span class="sr-only">Telegram</span>
+              <svg fill="currentColor" viewBox="0 0 24 24" class="h-6 w-6 fill-current" aria-hidden="true">
+                <path fill-rule="evenodd" d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z" clip-rule="evenodd"></path>
+              </svg>
+            </a>
+            <a href="https://twitter.com/BeepERC20" target="_blank" rel="noopener noreferrer">
+              <span class="sr-only">Twitter Projekt</span>
+              <svg fill="currentColor" viewBox="0 0 24 24" class="h-6 w-6 fill-current" aria-hidden="true">
+                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
+              </svg>
+            </a>
+              <a href="https://twitter.com/BeepRaider" target="_blank" rel="noopener noreferrer">
+              <span class="sr-only">Twitter Raid</span>
+              <svg fill="currentColor" viewBox="0 0 24 24" class="h-6 w-6 fill-current" aria-hidden="true">
+                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
+              </svg>
+            </a>
+          </div>
+          </div>
+            <Button type="button"
+              class="w-auto max-w-[25rem] inline-flex items-center justify-between text-dark border-dark rounded-full text-sm hover:text-gray-200 hover:bg-dark hover:cursor-copy overflow-hidden p-1 pr-2 vorder-2 contract"
+            >
+              <span class="sr-only">Click to copy ETH address</span>
+              <img :src="require('~/assets/img/logo/eth_logo.svg')" class="w-6 h-6">            
+              <span class="ml-2 text-sm w-auto text-ellipsis overflow-hidden">0xaa1656b7d4629476fa4cf76ccfbc01a4653bac71</span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-4 h-4 ml-1"><path d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z"></path><path d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z"></path></svg>
+            </Button>
+        </div>
+        <div class="hidden sm:block col-span-12 lg:col-span-4">
+          <div class="w-full">
+            <img
+              data-aos="fade-up"
+              data-aos-once="true"
+              :src="require('~/assets/img/logo/robot.png')"
+              class="-mt-4"
+              alt=""
+            />
+          </div>
+<vue-particles
+        color="#000000"
+        :particleOpacity="0.7"
+        linesColor="#000000"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="5"
+        :linesWidth="2"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+        </div>
+      </BaseSection>
+    </section>
 
-    <br>
 
 
-    <div class="wrap-code text-left">
-      <h4 class="white">
-        Main.js file
-      </h4>
-      <pre class="language-js"><code>
-import Vue from 'vue'
-import VueParticles from 'vue-particles'
-Vue.use(VueParticles)
-        </code></pre>
+
+    <div class="w-full my-10 flex justify-center">
+      <a
+        v-smooth-scroll
+        data-aos="flip-down"
+        data-aos-delay="150"
+        href="#navbar"
+        class="px-6 py-3 flex items-center space-x-2 bg-[#FAFAFA] hover:bg-gray-100 hover:shadow-md border border-[#DDDDDD] rounded-md text-gray-700"
+      >
+        <span>Back to top</span>
+        <ArrowUpIcon :size="20" />
+      </a>
     </div>
-    <br>
-    <div class="wrap-code text-left">
-      <h4 class="white">
-        App.vue file - Simple example
-      </h4>
-      <pre class="language-html"><code>
-{{appCode}}
-        </code></pre>
-    </div>
-    <br>
-
-    <div class="wrap-code text-left">
-      <h4 class="white">
-        App.vue file - Full example
-      </h4>
-      <pre class="language-html"><code>
-{{appCodeFull}}
-        </code></pre>
-    </div>
-    <br>
-
-    <div class="wrap-table">
-      <h4 class="white text-left">
-       Props
-      </h4>
-      <table class="rwd-table">
-
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Default</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-
-        <tr>
-          <td data-th="Name">color</td>
-          <td data-th="Type">String</td>
-          <td data-th="Default">#dedede</td>
-          <td data-th="Description">Particle color</td>
-        </tr>
-        <tr>
-          <td data-th="Name">particleOpacity</td>
-          <td data-th="Type">Number</td>
-          <td data-th="Default">0.7</td>
-          <td data-th="Description">Particle opacity</td>
-        </tr>
-        <tr>
-          <td data-th="Name">particlesNumber</td>
-          <td data-th="Type">Number</td>
-          <td data-th="Default">80</td>
-          <td data-th="Description">Amount of particles</td>
-        </tr>
-        <tr>
-          <td data-th="Name">shapeType</td>
-          <td data-th="Type">String</td>
-          <td data-th="Default">"circle"</td>
-          <td data-th="Description">
-            Available shape types: "circle","edge","triangle",
-            "polygon","star"
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">particleSize</td>
-          <td data-th="Type">Number</td>
-          <td data-th="Default">4</td>
-          <td data-th="Description">
-            Size of single particle
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">linesColor</td>
-          <td data-th="Type">String</td>
-          <td data-th="Default">#dedede</td>
-          <td data-th="Description">
-            Lines color
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">linesWidth</td>
-          <td data-th="Type">Number</td>
-          <td data-th="Default">1</td>
-          <td data-th="Description">
-            Lines width
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">lineLinked</td>
-          <td data-th="Type">Boolean</td>
-          <td data-th="Default">true</td>
-          <td data-th="Description">
-            Enable lines
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">lineOpacity</td>
-          <td data-th="Type">Number</td>
-          <td data-th="Default">0.4</td>
-          <td data-th="Description">
-            Line opacity
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">linesDistance</td>
-          <td data-th="Type">Number</td>
-          <td data-th="Default">150</td>
-          <td data-th="Description">
-            Lines distance
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">moveSpeed</td>
-          <td data-th="Type">Number</td>
-          <td data-th="Default">3</td>
-          <td data-th="Description">
-            Particles speed
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">hoverEffect</td>
-          <td data-th="Type">Boolean</td>
-          <td data-th="Default">true</td>
-          <td data-th="Description">
-            Enable hover effect
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">hoverMode</td>
-          <td data-th="Type">String</td>
-          <td data-th="Default">grab</td>
-          <td data-th="Description">
-            Available hover modes:
-            "grab", "repulse", "bubble"
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">clickEffect</td>
-          <td data-th="Type">Boolean</td>
-          <td data-th="Default">true</td>
-          <td data-th="Description">
-            Enable click effect
-          </td>
-        </tr>
-        <tr>
-          <td data-th="Name">clickMode</td>
-          <td data-th="Type">String</td>
-          <td data-th="Default">push</td>
-          <td data-th="Description">
-            Available click modes:
-            "push", "remove", "repulse", "bubble"
-          </td>
-        </tr>
-      </table>
-    </div>
-
-
-
-
   </div>
 </template>
+
 <script>
-  /* eslint-disable */
-  const appCode = `<template>
-      <div id="app">
-        <vue-particles color="#dedede"></vue-particles>
-      </div>
-   </template>`
-  const appCodeFull = `<template>
-      <div id="app">
-        <vue-particles
-          color="#dedede"
-          :particleOpacity="0.7"
-          :particlesNumber="80"
-          shapeType="circle"
-          :particleSize="4"
-          linesColor="#dedede"
-          :linesWidth="1"
-          :lineLinked="true"
-          :lineOpacity="0.4"
-          :linesDistance="150"
-          :moveSpeed="3"
-          :hoverEffect="true"
-          hoverMode="grab"
-          :clickEffect="true"
-          clickMode="push"
-        >
-        </vue-particles>
-      </div>
-   </template>`
-    /* eslint-disable */
-  export default {
-    name: 'main-content',
-    data () {
-      return {
-        appCode,
-        appCodeFull
-      }
+import aosMixin from '@/mixins/aos'
+export default {
+  name: 'app',
+  mixins: [aosMixin],
+  data() {
+    return {
+      selected: 0,
+      dropdownConcurency: false,
+      dropdownCrypto: false,
+      currencySelected: {
+        img: 'country-icon/eng.png',
+        name: 'USD',
+      },
+      currencies: [
+        {
+          img: 'country-icon/eng.png',
+          name: 'USD',
+        },
+      ],
+      cryptoSelected: {
+        img: 'crypto-icon/bitcoin.png',
+        name: 'BTC',
+      },
+      cryptocurrencies: [
+        {
+          img: 'crypto-icon/bitcoin.png',
+          name: 'BTC',
+        },
+      ],
+      trendings: [
+        {
+          id: 1,
+          name: 'Bitcoin',
+          price: 43180.13,
+          logo: 'bitcoin.png',
+          increase: true,
+          data: [40, 35, 60, 75, 60, 75, 50],
+        },
+        {
+          id: 2,
+          name: 'Ethereum',
+          price: 3480.65,
+          logo: 'ethereum.png',
+          increase: false,
+          data: [25, 30, 60, 50, 80, 55, 80],
+        },
+        {
+          id: 3,
+          name: 'Solana',
+          price: 150.2,
+          logo: 'solana.png',
+          increase: true,
+          data: [40, 45, 40, 80, 50, 60, 35],
+        },
+        {
+          id: 4,
+          name: 'Dogecoin',
+          price: 0.1572,
+          logo: 'dogecoin.png',
+          increase: true,
+          data: [35, 70, 60, 80, 50, 60, 40],
+        },
+      ],
+      topGainers: [
+        {
+          id: 1,
+          name: 'PAPPAY',
+          price: 0.00374,
+          logo: 'pappay.png',
+          increase: true,
+          data: [30, 50, 45, 60, 70, 40, 45],
+        },
+        {
+          id: 2,
+          name: 'Bitcoin Asia',
+          price: 0.02096,
+          logo: 'bitcoin-asia.png',
+          increase: true,
+          data: [25, 60, 50, 60, 35, 50, 70],
+        },
+        {
+          id: 3,
+          name: 'MoonRock',
+          price: 0.004907,
+          logo: 'moonrock.png',
+          increase: true,
+          data: [40, 35, 40, 25, 50, 70, 45],
+        },
+        {
+          id: 4,
+          name: 'NinjaFloki',
+          price: 0.000123,
+          logo: 'ninjafloki.png',
+          increase: true,
+          data: [45, 35, 40, 30, 25, 45, 35],
+        },
+      ],
+      recents: [
+        {
+          id: 1,
+          name: 'MetaCraft',
+          price: 0.0608,
+          logo: 'metacraft.png',
+          increase: false,
+          data: [40, 50, 45, 60, 35, 40, 45],
+        },
+        {
+          id: 2,
+          name: 'Frog',
+          price: 0.5875,
+          logo: 'frog.png',
+          increase: false,
+          data: [25, 50, 45, 48, 40, 60, 45],
+        },
+        {
+          id: 3,
+          name: 'Musk Doge',
+          price: 0.04041,
+          logo: 'musk-doge.png',
+          increase: true,
+          data: [25, 35, 60, 45, 50, 45, 45],
+        },
+        {
+          id: 4,
+          name: '2SHARE',
+          price: 1366.24,
+          logo: '2share.png',
+          increase: true,
+          data: [35, 30, 60, 50, 35, 45, 40],
+        },
+      ],
+      accordions: [
+        {
+          title: 'Why should I choose NEFA?',
+          description:
+            "We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over 8 million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.",
+        },
+        {
+          title: 'How secure is NEFA?',
+          description:
+            "We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.",
+        },
+        {
+          title: 'Do I have to buy a whole Bitcoin?',
+          description:
+            "We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.",
+        },
+        {
+          title: 'How do I actually buy Bitcoin?',
+          description:
+            "We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.",
+        },
+      ],
+      steps: [
+        {
+          img: 'sign-up.png',
+          title: 'Sign Up',
+          description:
+            'Sign up for your free NEFA Wallet on web, iOS or Android and follow our easy process to set up your profile',
+        },
+        {
+          img: 'fund.png',
+          title: 'Fund',
+          description:
+            'Choose your preferred payment method such as bank transfer or credit card to top up your NEFA Wallet',
+        },
+        {
+          img: 'buy-crypto.png',
+          title: 'Buy Crypto',
+          description:
+            'Buy Bitcoin or Ethereum, then securely store it in your Wallet or send it on easily to your friends anywhere',
+        },
+      ],
     }
-  }
+  },
+}
 </script>
+<style scoped>
+.text-header-gradient {
+  background: rgb(57, 132, 244);
+  background: linear-gradient(169.4deg, #3984f4 -6.01%, #0cd3ff 36.87%, #2f7cf0 78.04%, #0e65e8 103.77%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.bg-partner {
+  background: url('../assets/img/partner/background.png');
+  background-size: cover;
+  background-position: center;
+}
+.bg-trading-tools {
+  background: url('../assets/img/bg-trading-tools.webp');
+  background-size: cover;
+  background-position: center;
+}
+.max-h-0 {
+  max-height: 0;
+}
+</style>
